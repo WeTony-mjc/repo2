9aa154fcd180bdfa7ee4b5a829be1237a16e7a79
WeTony-mjc <mjc1479490506@163.com>
2020-08-20 09:42:30 +0800

    提交成功

---
+++ src/com/mjc/pojo/Hello.java
@@ -0,0 +1,11 @@
+package com.mjc.pojo;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+    }
+}
